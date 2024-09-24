2b5efcc276037432f2596d993f77cc7f0ccf5038
Bazine-Othmane <othmanebaz6@gmail.com>
2024-09-24 10:00:33 +0200

    second commit

---
+++ create_database.py
@@ -0,0 +1,94 @@
+# from langchain.document_loaders import DirectoryLoader
+from langchain_community.document_loaders import DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.schema import Document
+#from langchain.embeddings import OpenAIEmbeddings
+#from langchain_community.embeddings import OpenAIEmbeddings
+#from openai.embeddings_utils import OpenAIEmbeddings
+#from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+
+import openai 
+from dotenv import load_dotenv
+import os
+import shutil
+
+
+# Load environment variables. Assumes that project contains .env file with API keys
+
+#---- Set OpenAI API key 
+# Change environment variable name from "OPENAI_API_KEY" to the name given in 
+# your .env file.
+
+load_dotenv()
+openai.api_key = os.environ['OPENAI_API_KEY']
+
+CHROMA_PATH = "chroma"
+DATA_PATH = "data/books"
+
+
+def main():
+    generate_data_store()
+
+
+def generate_data_store():
+    print("start \n")
+    documents = load_documents()
+    print("\n finish load_documents \n")
+    chunks = split_text(documents)
+    print("\n finish split_text \n")
+    save_to_chroma(chunks)
+    print("\n finish all \n")
+    
+
+
+def load_documents():
+    loader = DirectoryLoader(DATA_PATH, glob="*.md")
+    documents = loader.load()
+    return documents
+
+
+def split_text(documents: list[Document]):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=300,
+        chunk_overlap=100,
+        length_function=len,
+        add_start_index=True,
+    )
+    chunks = text_splitter.split_documents(documents)
+    print(f"Split {len(documents)} documents into {len(chunks)} chunks.")
+
+    #document = chunks[10]
+    #print(document.page_content)
+    #print(document.metadata)
+
+    return chunks
+
+
+
+
+def save_to_chroma(chunks: list[Document]):
+    # Clear out the database first if it exists.
+    if os.path.exists(CHROMA_PATH):
+        shutil.rmtree(CHROMA_PATH)
+
+    # Initialize the OpenAI embeddings.
+    embeddings = OpenAIEmbeddings()
+
+    # Create a new Chroma DB from the documents.
+    db = Chroma.from_documents(
+        documents=chunks, 
+        embedding=embeddings, 
+        persist_directory=CHROMA_PATH
+    )
+    
+    # Persist the Chroma database.
+    db.persist()
+    print(f"Saved {len(chunks)} chunks to {CHROMA_PATH}.")
+
+if __name__ == "__main__":
+    main()
+
+    
+   
