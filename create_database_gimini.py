ddc786d473c27936767e8eb806e8aa512d7dfdf6
Bazine-Othmane <othmanebaz6@gmail.com>
2024-09-28 16:42:39 +0200

    with gimini

---
+++ create_database_gimini.py
@@ -0,0 +1,87 @@
+
+from langchain_community.document_loaders import DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.schema import Document
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+import openai 
+from dotenv import load_dotenv
+import os
+import google.generativeai as genai
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import shutil
+
+
+# Load environment variables. Assumes that project contains .env file with API keys
+
+#---- Set OpenAI API key 
+# Change environment variable name from "OPENAI_API_KEY" to the name given in 
+# your .env file.
+
+load_dotenv()
+genai.configure(api_key=os.environ['GOOGLE_API_KEY'])
+
+CHROMA_PATH = "chroma_gimini"
+DATA_PATH = "data/books"
+
+
+def main():
+    generate_data_store()
+
+def generate_data_store():
+    print("start \n")
+    documents = load_documents()
+    chunks = split_text(documents)
+    save_to_chroma(chunks)
+    
+
+
+def load_documents():
+    loader = DirectoryLoader(DATA_PATH, glob="*.md")
+    documents = loader.load()
+    return documents
+
+
+def split_text(documents: list[Document]):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=300,
+        chunk_overlap=100,
+        length_function=len,
+        add_start_index=True,
+    )
+    chunks = text_splitter.split_documents(documents)
+    print(f"Split {len(documents)} documents into {len(chunks)} chunks.")
+
+    #document = chunks[10]
+    #print(document.page_content)
+    #print(document.metadata)
+
+    return chunks
+
+
+
+
+def save_to_chroma(chunks: list[Document]):
+    # Clear out the database first if it exists.
+    if os.path.exists(CHROMA_PATH):
+        shutil.rmtree(CHROMA_PATH)
+
+    # Initialize the OpenAI embeddings.
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004")
+
+    # Create a new Chroma DB from the documents.
+    db = Chroma.from_documents(
+        documents=chunks, 
+        embedding=embeddings, 
+        persist_directory=CHROMA_PATH
+    )
+    
+    # Persist the Chroma database.
+    #db.persist()
+    print(f"Saved {len(chunks)} chunks to {CHROMA_PATH}.")
+
+if __name__ == "__main__":
+    main()
+
+    
+   
--- query_data_gimini.py
+++ query_data_gimini.py
@@ -1,19 +1,15 @@
 import argparse
-# from dataclasses import dataclass
-#from langchain_community.vectorstores import Chroma
 from langchain_chroma import Chroma
-
-from langchain_openai import OpenAIEmbeddings
-from langchain_openai import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
-import openai
 import os
+import google.generativeai as genai
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
 
 load_dotenv()
-openai.api_key = os.environ['OPENAI_API_KEY']
+genai.configure(api_key=os.environ['GOOGLE_API_KEY'])
 
-CHROMA_PATH = "chroma"
+CHROMA_PATH = "chroma_gimini"
 
 PROMPT_TEMPLATE = """
 Answer the question based only on the following context:
@@ -25,42 +21,42 @@ Answer the question based only on the following context:
 Answer the question based on the above context: {question}
 """
 
-
 def main():
-    # Create CLI.
-    """parser = argparse.ArgumentParser()
-    parser.add_argument("query_text", type=str, help="The query text.")
-    args = parser.parse_args()
-    query_text = args.query_text"""
-    query_text='how does Alice meet the mad hatter?'
+    query_text = 'Who are the animals that Alice met?'
+
+
+
+    embedding_function = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004")
+    #vector = embedding_function.embed_query(query_text)
+    #print(len(vector))
 
     # Prepare the DB.
-    embedding_function = OpenAIEmbeddings()
+  
     db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
 
     # Search the DB.
-    results = db.similarity_search_with_relevance_scores(query_text, k=3)
-    if len(results) == 0 or results[0][1] < 0.7:
+    results = db.similarity_search_with_relevance_scores(query_text, k=10)
+    relevance_scores = [result[1] for result in results]
+
+# Print the relevance scores
+    print(relevance_scores)
+    
+    if len(results) == 0:
         print(f"Unable to find matching results.")
         return
-   
-    
-    
+
     context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
     prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
     
     prompt = prompt_template.format(context=context_text, question=query_text)
-    print(prompt)
+    #print(prompt)
 
-    model = ChatOpenAI()
-    response_text = model.invoke(prompt)
+    model = genai.GenerativeModel('gemini-1.5-pro')
+    response = model.generate_content(prompt)
+    response_text = response.text
 
     sources = [doc.metadata.get("source", None) for doc, _score in results]
     formatted_response = f"Response: {response_text}\nSources: {sources}"
     print(formatted_response)
-    
-    
-
-
 if __name__ == "__main__":
     main()
